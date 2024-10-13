63c63d5dd3a241911f60ed08b66c12e188b470b6
P13R3042 <calamo.1883298@studenti.uniroma1.it>
2024-10-13 23:45:09 +0200

    Update scripts.py

--- HW-1/prob1/scripts.py
+++ HW-1/prob1/scripts.py
@@ -225,6 +225,219 @@ if __name__ == '__main__':
 ###################################################################
 #• Strings (all – total: 14 - max points: 220)
 #https://www.hackerrank.com/domains/python/py-strings
+#STRING SPLIT AND JOIN
+def split_and_join(line):
+    # split the string
+    words = line.split(" ")
+    
+    # merge strings
+    return "-".join(words)
+#--------------------------------------------------------
+#sWAP cASE
+def swap_case(s):    
+    return s.swapcase()
+
+#--------------------------------------------------------
+#What's Your Name?
+def print_full_name(first, last):
+    print(
+        "Hello "+first+" "+last+"! You just delved into python."
+    )
+
+#--------------------------------------------------------
+#Mutations
+def mutate_string(string, position, character):
+    str_lst = list(string)
+    str_lst[position] = character
+    return ''.join(str_lst)
+
+
+#--------------------------------------------------------
+#Find a string
+def count_substring(string, sub_string):
+    cnt = 0
+    i = 0
+    while i < len(string):
+        
+        pos = string.find(sub_string, i)
+
+        if pos != -1:
+            cnt += 1
+            i = pos + 1
+        else:
+            break
+
+    return cnt
+#--------------------------------------------------------
+#String Validators
+'''
+if __name__ == '__main__':
+    s = input()
+    print(any(char.isalnum() for char in s))
+    print(any(char.isalpha() for char in s))
+    print(any(char.isdigit() for char in s))
+    print(any(char.islower() for char in s))
+    print(any(char.isupper() for char in s))
+''' 
+
+
+#--------------------------------------------------------
+#Text Alignment
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c * i).rjust(thickness-1) + c + (c * i).ljust(thickness))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+#--------------------------------------------------------
+#Text Wrap
+def wrap(string, max_width):
+    
+    return textwrap.fill(string, max_width)
+
+#--------------------------------------------------------
+#Designer Door Mat
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def design_door_mat(n,m):
+    # parte superiore
+    for i in range(n // 2):
+        pattern = ('.|.' * (2 * i + 1)).center(m, '-')
+        print(pattern)
+    
+    # riga centrale
+    welcome_line = "WELCOME".center(m, '-')
+    print(welcome_line)
+    
+    # parte inferiore
+    for i in range(n // 2 - 1, -1, -1):
+        pattern = ('.|.' * (2 * i + 1)).center(m, '-')
+        print(pattern)
+
+if __name__ == '__main__':
+    # Input: N and M
+    input_ = input().strip()
+    n,m = map(int, input_.split())
+    design_door_mat(n,m)
+
+
+#--------------------------------------------------------
+#String Formatting
+def print_formatted(number):
+    #max witdh :=
+    width = len(bin(number)) - 2  # minus 2 for '0b'
+    
+    for i in range(1, number + 1):
+        # Format the output
+        decimal = str(i).rjust(width)
+        octal = oct(i)[2:].rjust(width)  # minus '0o'
+        hexadecimal = hex(i)[2:].upper().rjust(width)  # minus '0x' 
+        binary = bin(i)[2:].rjust(width)  # minus '0b'
+
+        print(f"{decimal} {octal} {hexadecimal} {binary}")
+
+
+#--------------------------------------------------------
+#Alphabet Rangoli
+import string
+def print_rangoli(size):
+    c = string.ascii_lowercase[:size]
+
+    rows = []
+    
+    # Create the lower part
+    for i in range(size - 1, 0, -1):
+        core_string = '-'.join(c[size-1:i:-1] + c[i:size])
+        rows.append(core_string.center(size * 4 - 3, '-'))
+
+
+    # Create the upper part including the middle row
+    for i in range(size):
+        core_string = '-'.join(c[size-1:i:-1] + c[i:size])
+        rows.append(core_string.center(size * 4 - 3, '-'))
+
+    print('\n'.join(rows))
+    return '\n'.join(rows)
+
+#--------------------------------------------------------
+#Capitalize!
+
+# Complete the solve function below.
+def solve(s):
+    res = s.split(' ')
+    res = [ i.capitalize() for i in res ]
+    return ' '.join(res)
+
+
+#--------------------------------------------------------
+#The Minion Game
+def minion_game(string):
+
+    kScore = 0
+    sScore = 0
+
+    for i in range(len(string)):
+
+        if string[i] in 'AEIOU':  # "Kevin has to make words starting with vowels. "
+        
+            # you'll have one subtring as string[i:] in string
+            # plus, you have to count all substring of "string" definend as:
+            #   string[i:x] where x goes from i to len(string) -1 
+            #
+            #consideration about multiple occurrence of a substring:
+            #   considering the example "BANANA", we notice that "AN" occours 2 times,
+            #       first : starting on index 1, second : starting on index 3
+            #   it is counted individually, each time the index "i" of the for goes in 1 and then in 3
+            kScore += len(string) - i  
+        else:  #" Stuart has to make words starting with consonants."
+            sScore += len(string) - i  # 
+    
+    # Determine the winner
+    if sScore > kScore :
+        print("Stuart "+str(sScore))
+    elif sScore < kScore:
+        print("Kevin "+str(kScore))
+    else:
+        print("Draw")
+
+
+
+#--------------------------------------------------------
+#Merge the Tools!
+def merge_the_tools(string, k):
+    substrs = [ string[i:i+k] for i in range(0,len(string) -k + 1,k) ]
+    res = []
+    tmp_word = "" 
+    for substr in substrs:
+        tmp_word = "" 
+        for i,c in enumerate(substr):
+            if i> 0 and c in substr[0:i]:
+                continue
+            tmp_word += c
+        print(tmp_word)
+        res.append(tmp_word)
+                
+        
+    
+
 ###################################################################
 #• Sets (all – total: 13 - max points: 170)
 #https://www.hackerrank.com/domains/python/py-sets
