e61a46d7c1842a74d46fa5bb1df5d2213c278201
P13R3042 <calamo.1883298@studenti.uniroma1.it>
2024-10-13 23:21:53 +0200

    Update scripts.py

--- HW-1/prob1/scripts.py
+++ HW-1/prob1/scripts.py
@@ -104,16 +104,123 @@ if __name__ == '__main__':
 #https://www.hackerrank.com/domains/python/py-basic-data-types
 
 #Nested Lists
+
+def nestedList(students):
+    
+    # Ordina gli studenti per voto (dal piu basso) e poi per nome
+    students_sorted = sorted(students, key=lambda x: x[1])
+    
+    # Trova il secondo voto piu basso
+    lowest_score = students_sorted[0][1]
+    second_lowest_score = None
+    
+    for student in students_sorted:
+        if student[1] > lowest_score:
+            second_lowest_score = student[1]
+            break
+    
+    # Trova tutti gli studenti con il secondo voto piu basso
+    second_lowest_students = [student[0] for student in students_sorted if student[1] == second_lowest_score]
+    
+    # Ordina i nomi in ordine alfabetico
+    second_lowest_students.sort()
+    
+    # Stampa i nomi
+    for student in second_lowest_students:
+        print(student)
+
+if __name__ == '__main__':
+    n = int(input())  # Numero di studenti
+    
+    students = []
+        
+    # Raccolta dei nomi e dei voti degli studenti
+    for _ in range(n):
+        name = input()  # Nome dello studente
+        score = float(input())  # Voto dello studente
+        students.append((name, score))
+    nestedList(students)
+
 #--------------------------------------------------------
 #List Comprehensions
+'''
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    result = [[i_z, i_y, i_x] for i_z in range(x + 1)
+                               for i_y in range(y + 1)
+                               for i_x in range(z + 1)
+                               if i_x + i_y + i_z != n]
+    
+    print(result)
+
+'''
 #--------------------------------------------------------
 #Find the Runner-Up Score!
+'''
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    unique_values = set(arr)
+    sorted_vals= list(unique_values)
+    sorted_vals.sort(reverse=True)
+
+
+    print(sorted_vals[1])
+'''
 #--------------------------------------------------------
 #Find the percentage
+'''
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    result = (sum(student_marks[query_name])/len(line))
+    print("%.2f" % result)
+'''
 #--------------------------------------------------------
 #Lists
+'''
+if __name__ == '__main__':
+    N = int(input())
+    res = []
+    for _ in range(N):
+        _input = input().split()
+        
+        match _input[0]:
+            case "insert":
+                res.insert(int(_input[1]), int(_input[2]))
+            case "print":
+                print(res)
+            case "remove":
+                res.remove(int(_input[1]))
+            case "append":
+                res.append(int(_input[1]))
+            case "sort":
+                res.sort()
+            case "pop":
+                res.pop()
+            case "reverse":
+                res.reverse()
+'''
 #--------------------------------------------------------
 #Tuples
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    val = input().strip()
+    val = list(map(int, val.split()))
+    print(hash(tuple(val)))
+    
+'''
 ###################################################################
 #• Strings (all – total: 14 - max points: 220)
 #https://www.hackerrank.com/domains/python/py-strings
